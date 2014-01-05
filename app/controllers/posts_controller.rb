3266eba5a76821d7aef85cdc8a21e15dc7fdc633
jersus <ryoko.vegetable@gmail.com>
2014-01-06 01:26:11 +0800

    refine tabs indent from 4 space to 2 space

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,44 @@
 class PostsController < ApplicationController
-    before_filter :find_post, :except => [ :index ]
+  before_filter :find_post, :except => [ :index ]
 
-    def index
-        @posts = Post.scoped
+  def index
+    @posts = Post.scoped
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    if @post.save
+      redirect_to( post_path( @post ), :flash => { :success => "created" } )
+    else
+      flash[ :error ] = @post.errors.full_messages
+      render :new
     end
+  end
 
-    def new
+  def update
+    if @post.update_attributes( params[ :post ] )
+      redirect_to( posts_path, :flash => { :success => "updated" } )
+    else
+      flash[ :error ] = @post.errors.full_messages
+      render :edit
     end
+  end
 
-    def edit
+  def destroy
+    @post.destroy
+    redirect_to( posts_path, :flash => { :success => 'deleted' } )
+  end
+
+  private
+    def find_post
+      @post = params[ :id ] ? Post.find( params[ :id ] ) : Post.new( params[ :post ] )
     end
-
-    def show
-    end
-
-    def create
-        if @post.save
-            redirect_to( post_path( @post ), :flash => { :success => "created" } )
-        else
-            flash[ :error ] = @post.errors.full_messages
-            render :new
-        end
-    end
-
-    def update
-        if @post.update_attributes( params[ :post ] )
-            redirect_to( posts_path, :flash => { :success => "updated" } )
-        else
-            flash[ :error ] = @post.errors.full_messages
-            render :edit
-        end
-    end
-
-    def destroy
-        @post.destroy
-        redirect_to( posts_path, :flash => { :success => 'deleted' } )
-    end
-
-    private
-        def find_post
-            @post = params[ :id ] ? Post.find( params[ :id ] ) : Post.new( params[ :post ] )
-        end
 end
