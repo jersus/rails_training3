60902080a5bf307aca61fa226494c2c38cab09e7
jersus <ryoko.vegetable@gmail.com>
2014-01-05 23:42:10 +0800

    implement #new #create #show of CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
     def index
         @posts = Post.scoped
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def show
+        @post = Post.find( params[ :id ] )
+    end
+
+    def create
+        @post = Post.new( params[:post] )
+        if @post.save
+            redirect_to( post_path( @post ), :flash => { :success => "created"} )
+        else
+            flash[ :error ] = @post.errors.full_messages
+            render :new
+        end
+    end
 end
